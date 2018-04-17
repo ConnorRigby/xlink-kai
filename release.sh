f1e6ed755cfcf2f34408c4945f8d8818730b9541
connor rigby <connor@farmbot.io>
2018-04-17 12:18:34 -0700

    Bump version

--- release.sh
+++ release.sh
@@ -3,7 +3,6 @@ TARGETS="rpi rpi0 rpi2 rpi3"
 rm -f *.img
 for i in $TARGETS; do
     echo "Building for: $i"
-    # MIX_TARGET=$i mix do deps.get, firmware, firmware.image
-    MIX_TARGET=$i mix do deps.get
-    # mv xlink.img xlink-$i-v$(cat VERSION).img
+    MIX_TARGET=$i mix do deps.get, firmware, firmware.image
+    mv xlink.img xlink-$i-v$(cat VERSION).img
 done
